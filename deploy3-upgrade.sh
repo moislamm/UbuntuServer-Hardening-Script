ea3487c74ecfe29252b71ed5ed28a139b663f843
moislamm <mohammed.islam@yahoo.co.uk>
2023-04-01 23:15:10 +0100

    Update deploy3-upgrade.sh

--- deploy3-upgrade.sh
+++ deploy3-upgrade.sh
@@ -1,47 +1,8 @@
 #!/bin/bash
 
 ##############################################################
-echo '===Opening up IPTables to allow intenet comms========='
-rm /etc/iptables/rules.v4
-echo '*filter' >> /etc/iptables/rules.v4
-echo ':INPUT DROP [130:14631]' >> /etc/iptables/rules.v4
-echo ':FORWARD DROP [0:0]' >> /etc/iptables/rules.v4
-echo ':OUTPUT ACCEPT [0:0]' >> /etc/iptables/rules.v4
-echo '#Allow loopback reverse telnet for Ser2Net connections' >> /etc/iptables/rules.v4
-echo '-A INPUT -i lo -j ACCEPT' >> /etc/iptables/rules.v4
-echo '-A OUTPUT -o lo -j ACCEPT' >> /etc/iptables/rules.v4
-echo '#Allow secured/whitelisted IPs on custom SSH port' >> /etc/iptables/rules.v4
-echo '-A INPUT -s 192.168.0.0/16 -p tcp -m tcp --dport 4044 -j ACCEPT' >> /etc/iptables/rules.v4
-echo '#The following two entires are only enabled for OS updates during scheduled CRON' >> /etc/iptables/rules.v4
-echo '-A INPUT -j ACCEPT' >> /etc/iptables/rules.v4
-echo '-A OUTPUT -j ACCEPT' >> /etc/iptables/rules.v4
-echo '#Drop everything else during normal operations' >> /etc/iptables/rules.v4
-echo '#-A INPUT -j DROP' >> /etc/iptables/rules.v4
-echo 'COMMIT' >> /etc/iptables/rules.v4
-iptables-restore < /etc/iptables/rules.v4
-echo '###DONE!###'
-##############################################################
 echo '===Performing update and system upgrades==============='
 sudo apt-get update
 sudo apt-get upgrade -y
 echo '###DONE!###'
 ##############################################################
-echo '===Re-instating Hardened IPtables======================'
-rm /etc/iptables/rules.v4
-echo '*filter' >> /etc/iptables/rules.v4
-echo ':INPUT DROP [130:14631]' >> /etc/iptables/rules.v4
-echo ':FORWARD DROP [0:0]' >> /etc/iptables/rules.v4
-echo ':OUTPUT ACCEPT [0:0]' >> /etc/iptables/rules.v4
-echo '#Allow loopback reverse telnet for Ser2Net connections' >> /etc/iptables/rules.v4
-echo '-A INPUT -i lo -j ACCEPT' >> /etc/iptables/rules.v4
-echo '-A OUTPUT -o lo -j ACCEPT' >> /etc/iptables/rules.v4
-echo '#Allow secured/whitelisted IPs on custom SSH port' >> /etc/iptables/rules.v4
-echo '-A INPUT -s 192.168.0.0/16 -p tcp -m tcp --dport 4044 -j ACCEPT' >> /etc/iptables/rules.v4
-echo '#The following two entires are only enabled for OS updates during scheduled CRON' >> /etc/iptables/rules.v4
-echo '#-A INPUT -j ACCEPT' >> /etc/iptables/rules.v4
-echo '#-A OUTPUT -j ACCEPT' >> /etc/iptables/rules.v4
-echo '#Drop everything else during normal operations' >> /etc/iptables/rules.v4
-echo '-A INPUT -j DROP' >> /etc/iptables/rules.v4
-echo 'COMMIT' >> /etc/iptables/rules.v4
-iptables-restore < /etc/iptables/rules.v4
-echo '###DONE!###'
